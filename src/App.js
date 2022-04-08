0836f9e988d3eb29f43bd7e232ee475de284a5e9
nnnnadia <tristaonadia@gmail.com>
2022-04-08 19:39:54 -0300

    Requisito 1: cria header com título e arquivos de estilização

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './App.css';
+import Header from './components/Header';
+import './style/App.css';
 
 class App extends React.Component {
   render() {
-    return (<div className='App'>
-      
-    </div>);
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
   }
 }
 
