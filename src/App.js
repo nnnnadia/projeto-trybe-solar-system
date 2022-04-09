df9b3246876cc1a9ce506f75d01364c2ea2f6ef1
nnnnadia <tristaonadia@gmail.com>
2022-04-09 10:52:34 -0300

    Requisito 7: cria o componente Missions e o renderiza em App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
 import './style/App.css';
 
@@ -9,6 +10,7 @@ class App extends React.Component {
       <div className="App">
         <Header />
         <SolarSystem />
+        <Missions />
       </div>
     );
   }
