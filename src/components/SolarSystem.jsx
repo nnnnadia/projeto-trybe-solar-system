7c2148ed49645da0ea872966dbeea212f012123f
nnnnadia <tristaonadia@gmail.com>
2022-04-09 10:47:49 -0300

    Requisito 6: renderiza PlanetCard em SolarSystem

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
+import '../style/SolarSystem.css';
 
 class SolarSystem extends Component {
   render() {
+    const planetsCards = planets
+      .map(({ name, image }, index) => (<PlanetCard
+        key={ index }
+        planetName={ name }
+        planetImage={ image }
+      />));
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <div className="planets-container">
+          {planetsCards}
+        </div>
       </div>
     );
   }
